abbf040f7c3504e0e5ea8f4299e4509978f82e41
samapikart <31254492+samapikart@users.noreply.github.com>
2017-09-11 19:17:40 +0530

    Create Problem2.cpp

---
+++ Problem2.cpp
@@ -0,0 +1,36 @@
+#include<iostream.h>
+#include<conio.h>
+void main()
+{
+	 clrscr();
+	 int a, b, c, big:
+		 cout<<'enter three number: ":
+		 cin>>a>>b>>c>>;
+		 //let a is the biggest
+		 big=a;
+		 if(big<b>
+		 {
+              if (b>c)
+              {
+              	        big=b;
+              }
+              else
+              {
+              	   big=c
+              }
+		 }
+		 else if (big<<c)
+		 {
+		 	     if(c>b)
+		          {
+		                big=c;
+		          }
+		          else
+		                  big=b;
+		 }
+}
+else 
+{
+	cout<<"biggest number is "<<big;
+	getch();
+}
