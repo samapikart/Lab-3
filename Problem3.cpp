e5453b39d93c130ba243070786f800e16d7a566f
samapikart <31254492+samapikart@users.noreply.github.com>
2017-09-11 19:18:35 +0530

    Create Problem3.cpp

---
+++ Problem3.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+/*program to determine whether the number is postive,negative or zero*/
+
+int main()
+{
+	floata=0;
+	cout<<"program to determine whether the number is postive,negative or zero"
+	cout<<"\n\nEnter the nuber";
+	cin>>a;
+	if(a>o)
+	cout<<'"\n\nThe number is postive";
+	else if(a<0)
+     cout<<"\nThe number is negative";
+     else
+     - cout<<"The number is 0";
+     + cout<<"The number is zero";
+     return 0;
+}
