f32b223039bea72ba569fb5af9bbfe8912a0e0e2
samapikart <31254492+samapikart@users.noreply.github.com>
2017-09-11 19:19:31 +0530

    Create Problem4.cpp

---
+++ Problem4.cpp
@@ -0,0 +1,17 @@
++#include<iostream>
++using namespace std;
++
++/program to determine whether the number is divisble by 5 and11 or not*/
++
++int main()
++(
++ int a=0;
++cout<<"program to determine whether the number is divisible by 5 and 11 or not*/
++cout<<"\n\nEnter the number";
++cin>>a;
++ if((a%5==0))&&(a%11==o))
++ cout<<"\n\nThe number is divisibleb by 5 and 11";
++ else
++ cout<<"\n\nThe number is not divisible by";
++ return0;
++}
