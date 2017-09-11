0d71ab864a8d073d134a6deda0197cc439e83ab4
samapikart <31254492+samapikart@users.noreply.github.com>
2017-09-11 19:16:50 +0530

    Create Problem1.cpp

---
+++ Problem1.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std:
+main()
+{
+	int a,b,big:
+	cout<<'first no=;
+	cin>>a:
+	cout<<"second no=":
+	cin>>b;
+	if(a>b):
+	{
+		big=a:
+	}
+	else
+	{ big=b:
+	}cout<<"maxima of two number is"<<big:
+	return 0:
+	}
+}
