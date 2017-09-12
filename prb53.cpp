f914135d8d540d577b727f48da4e7f76e4a86c4b
samapikart <31254492+samapikart@users.noreply.github.com>
2017-09-12 23:51:32 +0530

    Create prb53.cpp

---
+++ prb53.cpp
@@ -0,0 +1,30 @@
++#include <iostream>
+ +#include <cmath>
+ +using namespace std;
+ +
+ +/*PROGRAM TO PRINT ALL ARMSTRONG NUMBERS FROM 1 TO n*/
+ +
+ +int main()
+ +{
+ +  int num, num1, rem, sum = 0,dig,tnum;
+ +  cout<<"PROGRAM TO PRINT ALL ARMSTRONG NUMBERS FROM 1 TO n";
+ +  cout << "\n \nEnter a number: "<<endl;
+ +  cin >> num;
+ +  int i=1;
+ +  while(i<=num)
+ +  {
+ +  tnum=i;
+ +  sum=0;
+ +     while(tnum != 0)
+ +     {
+ +        dig = tnum % 10;
+ +        sum += pow(dig,3);
+ +        tnum /= 10;
+ +     }
+ +    if(sum == i)
+ +    cout<<"\n"<<i<< " is an Armstrong number.";
+ +    
+ +    i++;
+ +   }
+ +  return 0;
+ +}
