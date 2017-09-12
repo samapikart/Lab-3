ee1c5ff7c7ae538dd2ad1fae1357bcf38770f641
samapikart <31254492+samapikart@users.noreply.github.com>
2017-09-12 20:55:38 +0530

    Create problem5.cpp

---
+++ problem5.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+/*Program to determine whether the number is even or odd.*/
+
+int main()
+{
+  int a=0;
+  cout<<"Program to determine whether the number is even or odd..";
+  cout<<"\n \nEnter the number ";
+  cin>>a;
+  if((a%2==0))
+    cout<<"\n \nThe number is even ";
+  else
+    cout<<"\n \nThe number is odd";
+  return 0;
+}v
