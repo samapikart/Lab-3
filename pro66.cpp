cb2bf288195988c60008b26844358eb2c2eebcd0
samapikart <31254492+samapikart@users.noreply.github.com>
2017-09-13 01:01:19 +0530

    Create pro66.cpp

---
+++ pro66.cpp
@@ -0,0 +1,10 @@
+cpp
+ @@ -6,7 +6,7 @@ using namespace std;
+  
+  int main()
+  {
+ -    int n, binaryNumber;
+ +    int n;
+      cout<<"PROGRAM TO CONVERT DECIMAL TO BINARY";
+      cout << "\n \nEnter a decimal number: ";
+      cin >> n;
